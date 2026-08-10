8df9af7ddc15ae7ea870346b4c0ad1ec1319eec4
julzdao <julian.diaz.dev@proton.me>
2026-08-10 16:58:01 +0200

    fix re-usable component issue and add main logo

---
+++ projects/ui/src/lib/main-logo/main-logo.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'v42-main-logo',
+  imports: [],
+  templateUrl: './main-logo.component.html',
+  styleUrl: './main-logo.component.scss',
+})
+export class V42Logo {
+  @Input() clickable = true;
+  @Input() variant: 'default' | 'footer' = 'default';
+  @Output() navigate = new EventEmitter<{ notebook?: string }>();
+}
--- projects/ui/src/lib/ui.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-ui',
-  imports: [],
-  template: ` <p>ui works!</p> `,
-  styles: ``,
-})
-export class Ui {}
