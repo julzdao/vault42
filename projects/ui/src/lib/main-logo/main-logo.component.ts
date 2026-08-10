a775b47c9c32a70962de701d713e01172b6521ac
julzdao <julian.diaz.dev@proton.me>
2026-08-10 18:20:47 +0200

    improve main-logo content projection with required logoName

--- projects/ui/src/lib/main-logo/main-logo.component.ts
+++ projects/ui/src/lib/main-logo/main-logo.component.ts
@@ -8,6 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './main-logo.component.scss',
 })
 export class V42Logo {
+  @Input({required: true}) logoName!: string;
   @Input() clickable = true;
   @Input() variant: 'default' | 'footer' = 'default';
   @Output() navigate = new EventEmitter<{ notebook?: string }>();
